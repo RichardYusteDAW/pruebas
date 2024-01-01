ded78436d59335d56d5baf606fc33472492eb721
Richard Yuste <daw@richardyuste.com>
2024-01-01 18:46:24 +0100

    Added MultiplyTableFull

---
+++ ud2.practices/MultiplyTableFull.java
@@ -0,0 +1,17 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class MultiplyTableFull {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in).useLocale(Locale.US);
+
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 5; j <= 20; j++) {
+                System.out.printf(" %3d", i * j);
+            }
+            System.out.println();
+        }
+
+        in.close();
+    }
+}
