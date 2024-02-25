885bcb691e4003386a62911fdebddaf76f73e745
Richard Yuste <daw@richardyuste.com>
2024-02-25 16:43:00 +0100

    P4: Definició mètode puntuacio()

--- ud4.practices/BandurriaHero.java
+++ ud4.practices/BandurriaHero.java
@@ -3,7 +3,11 @@ package ud4.practices;
 import java.util.Scanner;
 
 public class BandurriaHero {
-    public static void main(String[] args){
+    public static int puntuacio(String notesCorrectes) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
     }
 }
