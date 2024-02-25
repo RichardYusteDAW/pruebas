ac9c9dac64fc7b34c3c5c2adbbe86813fab50046
Richard Yuste <daw@richardyuste.com>
2024-02-25 16:44:02 +0100

    P4: Processa entrada

--- ud4.practices/BandurriaHero.java
+++ ud4.practices/BandurriaHero.java
@@ -3,7 +3,12 @@ package ud4.practices;
 import java.util.Scanner;
 
 public class BandurriaHero {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        int nombreCasos = in.nextInt();
+
+        for (int i = 0; i < nombreCasos; i++) {
+            String liniaBandurria = in.nextLine();
+        }
     }
 }
